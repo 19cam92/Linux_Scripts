5451ade82a45c0e15c21b4c60c29a252f5b0b818
19cam92 <cameronfindlay123456789@gmail.com>
2018-10-26 22:07:19 +1100

    Added Script for installing Ubuntu Kernel Update Utility

---
+++ InstallUbuntuKernelUpdateUtility.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+printf '\033]2;%s\007' "Ubuntu Kernel Update Utility"
+
+echo "Installing Ubuntu Kernel Update Utility"
+sudo add-apt-repository ppa:teejee2008/ppa
+sudo apt-get update
+sudo apt-get install ukuu
+echo "Done!!!"
