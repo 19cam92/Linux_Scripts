9ccf8cecac0fc492509424e1135021ec0ed7ec88
19cam92 <cameronfindlay123456789@gmail.com>
2018-10-26 22:08:01 +1100

    Added script for installing Ubuntu Cleaner

---
+++ InstallUbuntuCleaner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+printf '\033]2;%s\007' "Ubuntu Cleaner"
+
+echo "Installing Ubuntu Cleaner"
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:gerardpuig/ppa
+sudo apt update
+sudo apt install ubuntu-cleaner
+echo "Done!!!"
